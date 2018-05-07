4678beb5d9ec676569bf95d7c300a4f896ed5faf
Matt Sanford <mtthwsanford@gmail.com>
2018-05-06 20:49:47 -0700

    Added comments to UI.js

--- UI.js
+++ UI.js
@@ -1,10 +1,22 @@
 (function(window){
 	function UI(){
 	  var tblID = "tblPuzzle";
+	  
+	  /*
+	  This will initialize the board's table ID value.
+	  If no value is given or if the function is not used the default "tblPuzzle" will be used
+	  */
 	  this.init = function(_tblID){
-	    tblID = _tblID;
+	    var id = _tblID;
+	    if(id){
+	      tblID = _tblID;
+	    }
 	  }
 	  
+	  /*
+	  This function will draw a HTML board given an array of values.
+	  The number of values MUST have an even square root
+	  */
 		this.DrawBoard = function(brdArray){
 		  var rowLength = Math.sqrt(brdArray.length);
 		  var tbl = document.getElementById(tblID);
@@ -18,9 +30,15 @@
 		  }
 		}
 		
+		/*
+		This will take a row object and a set of values for that row and the row's index on the board.
+		It creates a visual representation of the values in a table cell with an ID of that cells coordinates.
+		This can be modified to create images, divs, or any other html element
+		*/
 		function DrawRow(row, values, rowIndex){
 		  for(var i = 0; i < values.length; i++){
 		    var cell = row.insertCell(i);
+		    //This is where the object is made, any HTML element can work.
 		    cell.innerHTML = "<input type='button' id='" + rowIndex + i + "' value='" + values[i] + "'>";
 		  }
 		}
